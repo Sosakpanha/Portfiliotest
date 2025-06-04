8d5f9d91f082dacc9255f4c2a9a440c70146326b
Sosakpanha Menh <131564407+Sosakpanha@users.noreply.github.com>
2025-06-04 22:20:47 +0700

    feat(navbar): restructure links and responsive menu

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,14 +1,24 @@
 <template>
   <nav class="navbar">
-    <router-link to="/">Home</router-link>
-    <router-link to="/projects">Projects</router-link>
-    <router-link to="/about">About</router-link>
-    <router-link to="/contact">Contact</router-link>
+    <button class="menu-toggle" aria-label="Toggle navigation" @click="toggleMenu">
+      Menu
+    </button>
+    <ul :class="{ open: isOpen }">
+      <li><router-link to="/">Home</router-link></li>
+      <li><router-link to="/about">About</router-link></li>
+      <li><router-link to="/projects">Projects</router-link></li>
+      <li><router-link to="/contact">Contact</router-link></li>
+    </ul>
   </nav>
 </template>
 
 <script setup lang="ts">
-// Navigation logic can be added here
+import { ref } from 'vue'
+
+const isOpen = ref(false)
+const toggleMenu = () => {
+  isOpen.value = !isOpen.value
+}
 </script>
 
 <style scoped>
@@ -17,14 +27,48 @@
   padding: 1rem;
 }
 
+
+.menu-toggle {
+  display: none;
+  background: none;
+  border: none;
+  color: white;
+  font-size: 1.2rem;
+}
+
+.navbar ul {
+  display: flex;
+  list-style: none;
+  margin: 0;
+  padding: 0;
+}
+
+.navbar li {
+  margin: 0 1rem;
+}
+
 .navbar a {
   color: white;
-  margin: 0 1rem;
   text-decoration: none;
 }
 
 .navbar a.router-link-active {
   font-weight: bold;
   text-decoration: underline;
+}
+
+@media (max-width: 600px) {
+  .menu-toggle {
+    display: block;
+  }
+
+  .navbar ul {
+    display: none;
+    flex-direction: column;
+  }
+
+  .navbar ul.open {
+    display: flex;
+  }
 }
 </style>

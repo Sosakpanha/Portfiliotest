765af22cd0a3937d9412524ec98e138923bce7a9
Sosakpanha Menh <131564407+Sosakpanha@users.noreply.github.com>
2025-06-05 01:09:29 +0700

    Add SkillItem component

---
+++ src/components/SkillItem.vue
@@ -0,0 +1,32 @@
+<script setup lang="ts">
+defineProps<{
+  icon: string
+  name: string
+}>()
+</script>
+
+<template>
+  <div class="skill-item">
+    <img :src="icon" :alt="name + ' icon'" class="skill-icon" />
+    <span class="skill-name">{{ name }}</span>
+  </div>
+</template>
+
+<style scoped>
+.skill-item {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+
+.skill-icon {
+  width: 40px;
+  height: 40px;
+  object-fit: contain;
+}
+
+.skill-name {
+  margin-top: 0.5rem;
+  text-align: center;
+}
+</style>

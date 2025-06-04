f4c58422a6ab3ece3108c7984d525f83b89b2ad9
Sosakpanha Menh <131564407+Sosakpanha@users.noreply.github.com>
2025-06-04 22:51:53 +0700

    Add route transition wrapper

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,11 @@
 <template>
   <div id="app">
     <NavBar />
-    <router-view />
+    <router-view v-slot="{ Component }">
+      <transition name="fade" mode="out-in">
+        <component :is="Component" />
+      </transition>
+    </router-view>
   </div>
 </template>
 
@@ -16,5 +20,15 @@ import NavBar from './components/NavBar.vue'
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
+}
+
+.fade-enter-active,
+.fade-leave-active {
+  transition: opacity 0.3s ease;
+}
+
+.fade-enter-from,
+.fade-leave-to {
+  opacity: 0;
 }
 </style>

99fce1d956dff2560286e0c0790723af8cb75b4c
Sosakpanha Menh <131564407+Sosakpanha@users.noreply.github.com>
2025-06-05 20:29:29 +0700

    Wrap router view in full-screen container

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,13 @@
 <template>
   <div id="app">
     <NavBar />
-    <router-view v-slot="{ Component }">
-      <transition name="fade" mode="out-in">
-        <component :is="Component" />
-      </transition>
-    </router-view>
+    <div class="view-container">
+      <router-view v-slot="{ Component }">
+        <transition name="fade" mode="out-in">
+          <component :is="Component" />
+        </transition>
+      </router-view>
+    </div>
   </div>
 </template>
 
@@ -30,5 +32,14 @@ import NavBar from './components/NavBar.vue'
 .fade-enter-from,
 .fade-leave-to {
   opacity: 0;
+}
+
+.view-container {
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100vw;
+  height: 100vh;
+  overflow-y: auto;
 }
 </style>
